92f9a24eb14daa55c53c575ba6cfa80779e47aec
HieuPahmTr <phamvanthang786@gmail.com>
2025-04-12 16:45:38 +0700

    Initial commit

---
+++ src/main/java/vn/hust/AniheiU/AniheiUApplication.java
@@ -0,0 +1,11 @@
+package vn.hust.AniheiU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AniheiUApplication {
+        public static void main(String[] args) {
+        SpringApplication.run(AniheiUApplication.class, args);
+    }
+}
