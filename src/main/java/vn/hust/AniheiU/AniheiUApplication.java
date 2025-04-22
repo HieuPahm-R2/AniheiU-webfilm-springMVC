94eee2dad5144c323328a87d1f793172a4bbd581
HieuPahmTr <phamvanthang786@gmail.com>
2025-04-22 18:05:46 +0700

    change server port

--- src/main/java/vn/hust/AniheiU/AniheiUApplication.java
+++ src/main/java/vn/hust/AniheiU/AniheiUApplication.java
@@ -1,11 +1,17 @@
 package vn.hust.AniheiU;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AniheiUApplication {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(AniheiUApplication.class, args);
+    }
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
     }
 }
